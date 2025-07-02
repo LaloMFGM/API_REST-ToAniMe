38fb523a630951e5f0c5e976a00bd51dd9bf9615
LaloMFGM <lalomfgm@gmail.com>
2025-07-01 18:00:07 -0600

    ToAniMe base code is ready

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,27 @@
 import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
+import { router as routes } from "./routes/app.routes";
 
+
+
+// Load environment variables from .env file
 dotenv.config();
-
-
 const app = express();
 
 
+//Application use
 app.use(express.json());
+app.use(cors());
+app.use("/api", routes);
+
+// Import Middleware
+
+
+// Import Routes
+
+
+
 
 
 app.get('/', (req, res) => {
